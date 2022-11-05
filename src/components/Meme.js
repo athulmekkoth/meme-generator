829eb2b00c2fd101f94fed72a0a4ac677de8a5f1
athulmekkoth <athulmekkoth21@gmail.com>
2022-11-05 22:49:45 +0530

    fininsh

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,47 +1,54 @@
 import React from "react"
 import  ReactDOM  from "react-dom"
-import memesData from "./Memedata.js"
 
 export default function Meme() {
     /**
-     * Challenge: Update our state to save the meme-related
-     * data as an object called `meme`. It should have the
-     * following 3 properties:
-     * topText, bottomText, randomImage.
+     * Challenge: 
+     * As soon as the Meme component loads the first time,
+     * make an API call to "https://api.imgflip.com/get_memes".
      * 
-     * The 2 text states can default to empty strings for now,
-     * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+     * When the data comes in, save just the memes array part
+     * of that data to the `allMemes` state
      * 
-     * Next, create a new state variable called `allMemeImages`
-     * which will default to `memesData`, which we imported above
+     * Think about if there are any dependencies that, if they
+     * changed, you'd want to cause to re-run this function.
      * 
-     * Lastly, update the `getMemeImage` function and the markup 
-     * to reflect our newly reformed state object and array in the
-     * correct way.
+     * Hint: for now, don't try to use an async/await function.
+     * Instead, use `.then()` blocks to resolve the promises
+     * from using `fetch`. We'll learn why after this challenge.
      */
-     const [meme, setMeme] = React.useState({
+    
+    const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
+    const [allMemes, setAllMemes] = React.useState([])
     
-    const [memeImage, setMemeImage] = React.useState("http://i.imgflip.com/1bij.jpg")
-    
-    
+    React.useEffect(()=>{
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res=>res.json())
+            .then(data=>setAllMemes(data.data.memes))
+        
+    },[])
     function getMemeImage() {
-        const memesArray = meme.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-      const url = memesArray[randomNumber].url
-      setMeme(previmg=>({
-
-        ...previmg,
-        randomImage:url
-
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
         
-      }))
     }
-      
-        
+    
+    
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
     
     return (
         <main>
@@ -50,11 +57,17 @@ export default function Meme() {
                     type="text"
                     placeholder="Top text"
                     className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                 />
                 <input 
                     type="text"
                     placeholder="Bottom text"
                     className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
                 <button 
                     className="form--button"
@@ -63,7 +76,11 @@ export default function Meme() {
                     Get a new meme image 🖼
                 </button>
             </div>
-            <img src={meme.randomImage} className="meme--image" />
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
